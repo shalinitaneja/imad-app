1f8680c618dc526eac5a82695f411a134ea20877
shalinitaneja <30628496+shalinitaneja@users.noreply.github.com>
2017-09-07 22:11:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-  // res.send('<h1><font color=red><hr size=2>article one shown here<h1>') ;
+  
   res.sendFile(path.join(_dirname,'ui','article-one.html'));
   
   });
