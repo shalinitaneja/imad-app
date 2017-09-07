f6be48194929a7795ae4ede5fb4f0a96d2513930
shalinitaneja <30628496+shalinitaneja@users.noreply.github.com>
2017-09-07 20:52:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('article-one.html',function(req,res){
+   res.send('article one shown here') ;
+   res.send('<h1>hello friends</h1>');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
