501877f91388cabae23e857e598b873945e67d07
shalinitaneja <30628496+shalinitaneja@users.noreply.github.com>
2017-09-28 22:07:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var a=10;
 var img1=document.getElementById("one");
 function mm(){
     a=a+10;
-    img1.style.height="'"+a+"px+"'";
+    img1.style.height = "'"+a+"px"+"'";
 }
 img1.onclick=function(){
     var interval=setInterval(mm,5);
