3c300e53f2abda1b9bf2cda09c56169ab8dca6f4
shalinitaneja <30628496+shalinitaneja@users.noreply.github.com>
2017-09-28 16:04:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,45 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var atricleTwo={
+    title:"tile for article-two",
+    heading:"ARTICLE TWO",
+    date:"sept 25, 2017",
+    content:`
+    <p>first line of article two</p>
+    <p>second line of article two</p>
+    <p>third  line of article two</p>'
+    };
+    
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var data=data.date;
+    var content=data.content;
+    var htmlTemplate="<html> \
+    <head><title> ${title} </title> \
+    </head>  \
+    <body> \
+    <div>  \
+      <a href='/'>HOME</a>  \
+    </div>   \
+    <hr>   \
+    <h3>${heading}</h3>   \
+    <div>${date}</div>    \
+    <div>${content}</div>  \
+    </body>   \
+    </html> ";
+    return htmlTemplate;
+}
+    
+    
+    
+    
+    `
+
+
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -19,6 +58,9 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+app.get('/article-two', function (req, res) {
+  res.send(createTemplate(articleTwo);
 });
 
 
