2ad0601458ca2046a8d92d603f1369ea9c752764
shalinitaneja <30628496+shalinitaneja@users.noreply.github.com>
2017-09-28 22:26:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,12 @@
 console.log('Loaded!');
 var a=10;
-var img1=document.getElementById("one");
+var img1=document.getElementById("madi");
 console.log(a);
 function tt(){
     a=a+10;
-    img1.style.height = "'"+a+"px"+"'";
+  //  img1.style.height = "'"+a+"px"+"'";
+     img1.style.height = "300px";
+    
 }
 img1.onclick=function(){
     var interval=setInterval(tt,5);
