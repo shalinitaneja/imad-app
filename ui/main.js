d444870794a1cb2abacd4bf1f966e5f8d6f37dd1
shalinitaneja <30628496+shalinitaneja@users.noreply.github.com>
2017-09-28 20:38:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,8 @@ console.log('Loaded!');
 var a=10;
 var img1=document.getElementById('one');
 function mm(){
-    
-    img1.style.marginLeft= a+10+"px";
+a=a+10;
+    img1.style.marginLeft= a+"px";
 }
 img1.onClick=function(){
     var interval=setinterval(mm,5);
