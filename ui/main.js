f5a3e861744464461040e4c8e64d2ef5879c33f1
shalinitaneja <30628496+shalinitaneja@users.noreply.github.com>
2017-09-28 20:29:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,8 @@
 console.log('Loaded!');
 var a=10;
+var img1=document.getElementById('one');
 function mm(){
-    var img1=document.getElementById('one');
+    
     img1.height= img1.height+10;
 }
 img1.onClick(function(){
