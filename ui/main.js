1f52d8eab7557b52351860201bfb964567476577
shalinitaneja <30628496+shalinitaneja@users.noreply.github.com>
2017-10-05 12:46:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,3 @@
-/*console.log('Loaded!');
-var a=10;
-var img1=document.getElementById("madi");
-console.log(a);
-function tt(){
-    a=a+10;
-  //  img1.style.height = "'"+a+"px"+"'";
-     img1.style.height = "300px";
-    
-}
-img1.onclick=function(){
-    var interval=setInterval(tt,5);
-}; */
 // for counter objects
 var button=document.getElementById("counter");
 button.onclick=function()
