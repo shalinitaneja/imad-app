b53ba2036e09baebdb47f528e7f480e931f1d7f6
shalinitaneja <30628496+shalinitaneja@users.noreply.github.com>
2017-10-05 19:30:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,6 +64,10 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/article-two', function (req, res) {
   res.send(createTemplate(articleTwo));
 });
