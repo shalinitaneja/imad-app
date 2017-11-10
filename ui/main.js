51213c666b5637733ba273f2647a24c12e65f6d2
shalinitaneja <30628496+shalinitaneja@users.noreply.github.com>
2017-11-10 18:40:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,7 @@ var counter=0;
 var button=document.getElementById('counter');
 button.onclick=function()
 {
+  alert('hello');
    counter=counter+1;
    var abc=document.getElementById('count');
    abc.innerHtml=counter.toString();
